68d2c5f208ee8a93980b31d701bd89cfc3e16fd3
Sathish Kumar K- 910819104013 <113568315+SathishKumar013@users.noreply.github.com>
2022-11-19 11:24:09 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint 3/Flask/ai.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import os
+import copy
+import itertools
+import json
+import numpy as np
+from keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import tensorflow as tf
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from keras.models import model_from_json
+from flask import redirect, url_for
+from PIL import Image
+
+app=Flask(__name__)
+
+json_file = open('C:/Users/admin/Desktop/Flask/final.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+loaded_model.load_weights("C:/Users/admin/Desktop/Flask/final.h5")
+
+@app.route('/')
+def index():
+    return render_template('explore.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(basepath, '', secure_filename(f.filename))
+        f.save(file_path)
+        img = image.load_img(file_path, target_size=(64, 64))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        pred = np.argmax(loaded_model.predict(x), axis=-1)
+        op = ['Corpse Flower','Great Indian Bustard Bird', 'Lady Slipper Orchid Flower',
+              'Pangolin Mammal', 'Senenca White Deer Mammal','Spoon Billed Sandpiper Bird']
+        text = op[pred[0]]
+        return render_template('predict.html',value=text)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8000,debug=True)
+
